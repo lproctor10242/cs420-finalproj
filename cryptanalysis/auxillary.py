bce346cbf3bc22748a99604bc4eac55eae0f9aa0
sam dahms <90362989+sdahms@users.noreply.github.com>
2023-04-26 02:44:47 -0400

    preparations for data gathering

--- cryptanalysis/auxillary.py
+++ cryptanalysis/auxillary.py
@@ -1,21 +1,22 @@
 import os
 
 def main_call() -> None:
-    for pkl in [8,16,24]:
+    for pkl in [4,8,12,16]:
         for bc in [2,3,4,5]:
             for n in [32,64,128,256]:
-                for pc in [0.01, 0.1, 0.5, 0.9]:
+                for pc in [0.001, 0.01, 0.1, 0.33]:
                     for i in range(25):
-                        csv_out = f'{i+1}-k{pkl}-bc{bc}-n{n}-pc{pc}'
-                        print('python3 -m cryptanalysis.main' + 
+                        csv_out = f'{i+1}-key_len{pkl}-block_count{bc}-population{n}-crossover_chance{pc}'
+                        os.system('python3 -m cryptanalysis.main' + 
                                 " --pub_key_l " + str(pkl) +
-                                " --num_blocks " + bc +
+                                " --num_blocks " + str(bc) +
                                 " --pop " + str(n) +
-                                " --p_c " + str(pc) +
+                                " --p_m " + str(pc) +
                                 " --csv_output " + csv_out + 
-                                " --verbose True"
+                                " --verbose False"
                                 )
 
+                        print ( f'Number #{i+1}, key_len{pkl}, block_count{bc}, population{n}, crossover_chance{pc} COMPLETE!' )
 if __name__ == '__main__':
     main_call()
     
